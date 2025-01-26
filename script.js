c4d288fabb8025d0a874b7d0460fe71d4590fea2
Toafeek-Abbey <101677385+Toafeek-Abbey@users.noreply.github.com>
2025-01-26 20:58:06 +0100

    Update script.js

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ document.addEventListener('DOMContentLoaded', function () {
                   }
                   addButton.addEventListener("click", addTask)
                   taskInput.addEventListener('keypress',
-                      if (event.key == 'ENter') {
+                      if (event.key == "Enter") {
                          addTask()
                       }
                   )
